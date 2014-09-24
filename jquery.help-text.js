372cd87a8cc62711bee8da38572866a573ce6dd6
Robert Campbell <robert@deterset.com>
2014-09-24 16:22:27 -0400

    Update jquery.help-text.js

--- jquery.help-text.js
+++ jquery.help-text.js
@@ -6,14 +6,12 @@
  *-------------------------------------------------
 	$(document).ready(function() {
 		$('#username').helpText({
-			helpClass: 'dimmed',
 			text: 'email@host.com',
 			enter: 'tab',
 			focus: 1
 		});
 
 		$('#password').helpText({
-			helpClass: 'dimmed',
 			text: 'password'
 		});
 	});
@@ -22,19 +20,41 @@
  * @author deterset (Robert Campbell)
  * @homepage http://deterset.com
  * @created 2014-07-14
- * @version 0.1
+ * @version 0.3 - Tweek the init() function to setup each input field with the
+ *                dimmed help text. Added jquery.help-text.css to default the
+ *                dimmed help text class. init can also set the focus if
+ *                requested.
+ * @version 0.2 - initial working realease
  */
 (function($) {
+
     $.fn.helpText = function(options) {
 
         // merges the options w/defaults
         var settings = $.extend({
-            helpClass: 'none',
+            helpClass: 'helptext_dimmed',
             text: '',
-            enter: 'submit',			// option 'submit'(default browser behaviour)
+            enter: 'submit',            // option 'submit'(default browser behaviour)
                                         //     or 'tab' to next input field
-            focus: 0					// 1                  after page load focus this input
+            focus: 0                    // 1   after page load focus this input
         }, options);
+
+        this.init = function () {
+            $(this).each(function() {
+                if($(this).val() === '') {
+                  // adds the helpText back in
+                  $(this).val(settings.text);
+                  // adds the helpClass
+                  $(this).addClass(settings.helpClass);
+                }
+                $(this).attr("helpText",settings.text);
+                if (settings.focus) {
+                    this.focus();
+                    this[0].setSelectionRange(0, 0);
+                    settings.focus = 0;
+                }
+            });
+        };
 
         // clean off input help text before submitting form.
         function remove() {
@@ -46,14 +66,14 @@
             });
         }
 
-        $(this).closest("form").addClass('helpTextFormClass');
+        this.closest("form").addClass('helpTextFormClass');
         $(document).on('submit', 'form.helpTextFormClass', function(e) {
             remove();
         });
 
 
         // Prevent Form from being submitted on enter
-        $(this).closest("form").keypress(function(e) {
+        this.closest("form").keypress(function(e) {
             return e.keyCode != 13;
         });
 
@@ -101,7 +121,7 @@
 
         // when the user leaves the field return helpText if empty
         this.blur(function() {
-            if($(this).val() == '') {
+            if($(this).val() === '') {
               // adds the helpText back in
               $(this).val(settings.text);
               // adds the helpClass
@@ -109,21 +129,10 @@
             }
         });
 
-        // sets the default help text and helpClass
-        // also store the init text
-        if(this.val() == "") {
-            this.val(settings.text);
-            if (settings.helpClass != 'none') this.addClass(settings.helpClass);
-            if (this.attr('helpText') == undefined)
-                this.attr('helpText',settings.text);
-            if (settings.focus) {
-                this.focus();
-                this[0].setSelectionRange(0, 0);
-                settings.focus = 0;
-            }
-        }
+        // Initialize the plugin instance
+        this.init();
 
         // allows for jQuery chaining
-        return this;
+        return $(this);
     };
 }(jQuery));
